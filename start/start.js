9c232a8c9886840911000a91b6a36b7395684a21
d-smith <doug.smith.mail@gmail.com>
2018-11-01 17:59:49 -0700

    Added workflow start

--- start/start.js
+++ start/start.js
@@ -1,8 +1,37 @@
 
 const stateMachineArn = process.env.STATE_MACHINE_ARN;
+const workflowDomain = process.env.WORKFLOW_DOMAIN;
+const workflowType = process.env.WORKFLOW_TYPE;
+const workflowVersion = process.env.WORKFLOW_VERSION;
 
+const AWS = require('aws-sdk');
+const swf = new AWS.SWF();
+
+const chance = require('chance').Chance();
 
 module.exports.handler = async (event, context) => {
+
+    let id = chance.guid();
+    let response = await swf.startWorkflowExecution(
+        {
+            domain: workflowDomain,
+            workflowId: id,
+            workflowType: {
+                name: workflowType,
+                version: workflowVersion
+            },
+            childPolicy: 'REQUEST_CANCEL',
+            taskList: {
+                name: 'msflow'
+            },
+            input: '',
+            taskStartToCloseTimeout: 'NONE',
+            executionStartToCloseTimeout: `${60 * 60 * 24 * 180}` //180 days in seconds
+        }
+    ).promise();
+
+    console.log(JSON.stringify(response));
+
     console.log(`state machine arn: ${stateMachineArn}`);
     return 'ok';
 }
