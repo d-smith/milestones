b2f8452e57f590a64408c7d9a82389b4bdff56a9
Smith, Doug <doug.smith@fmr.com>
2018-11-01 17:59:00 -0700

    more tweaks

--- start/start.js
+++ start/start.js
@@ -4,5 +4,5 @@ const stateMachineArn = process.env.STATE_MACHINE_ARN;
 
 module.exports.handler = async (event, context) => {
     console.log(`state machine arn: ${stateMachineArn}`);
-    return 'ok';
+    return {statusCode: 200};
 }
