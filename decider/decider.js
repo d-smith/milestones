a5f0a688d50abef732a8192e6bb71e41583edf6b
d-smith <doug.smith.mail@gmail.com>
2018-11-03 14:21:40 -0700

    Update to start first step function orchestration

--- decider/decider.js
+++ decider/decider.js
@@ -6,6 +6,7 @@ const workflowTasklist = process.env.WORKFLOW_TASKLIST;
 
 const AWS = require('aws-sdk');
 const swf = new AWS.SWF();
+const stepfn = new AWS.StepFunctions();
 const _ = require('lodash');
 
 const makeid = () => {
@@ -20,50 +21,67 @@ const makeid = () => {
 
 const deciderId=makeid();
 
-const scheduleFirstActivity = async (events) => {
+const firstActivityShouldBeScheduled = async (events) => {
     let filtered = _.filter(events, (o) => { return o.eventType != 'DecisionTaskStarted'
                          && o.eventType != 'DecisionTaskScheduled'
                         && o.eventType != 'DecisionTaskTimedOut' });
     return filtered.length == 1;
 }
 
+const getExecutionId = (task) => {
+    return task.workflowExecution.workflowId;
+};
+
+const scheduleFirstActivity = async (task) => {
+    console.log('schedule activity 1');
+    return await swf.respondDecisionTaskCompleted({
+        taskToken: task.taskToken,
+        decisions: [
+            {
+                decisionType: 'ScheduleActivityTask',
+                scheduleActivityTaskDecisionAttributes: {
+                    activityId: `milestone1-act-${makeid()}`,
+                    activityType: {
+                        name: 'milestone1',
+                        version: '1'
+                    },
+                    control: 'control relevant data',
+                    input: 'activity input',
+                    scheduleToCloseTimeout: `${7 * 24 * 60 * 60}`,
+                    taskList: {
+                        name: 'milestone-activity-tasklist'
+                    },
+                    scheduleToStartTimeout: `${24 * 60 * 60}`,
+                    startToCloseTimeout: `300`,
+                    heartbeatTimeout: '300'
+                }
+            }
+        ]
+    }).promise();
+    
+};
+
+const startStepFunctionOrchestration = async (task) => {
+    let workflowId = getExecutionId(task);
+    console.log('start step function with execution id', workflowId);
+    
+    return await stepfn.startExecution({
+        stateMachineArn: stateMachineArn,
+        input: JSON.stringify({workflowId: workflowId})
+    }).promise();
+}
+
 const performDecisionTask = async (task) => {
     console.log(JSON.stringify(task));
     console.log('schedule first activity?')
-    if(scheduleFirstActivity(task.events)) {
-        //schedule first activity
-        console.log('schedule activity 1');
-        let response = await swf.respondDecisionTaskCompleted({
-            taskToken: task.taskToken,
-            decisions: [
-                {
-                    decisionType: 'ScheduleActivityTask',
-                    scheduleActivityTaskDecisionAttributes: {
-                        activityId: `milestone1-act-${makeid()}`,
-                        activityType: {
-                            name: 'milestone1',
-                            version: '1'
-                        },
-                        control: 'control relevant data',
-                        input: 'activity input',
-                        scheduleToCloseTimeout: `${5 * 60}`,
-                        taskList: {
-                            name: 'milestone-activity-tasklist'
-                        },
-                        scheduleToStartTimeout: `${24 * 60 * 60}`,
-                        startToCloseTimeout: `300`,
-                        heartbeatTimeout: '300'
-                    }
-                }
-            ]
-        }).promise();
-
-        
-        console.log(`schedule activity response: ${JSON.stringify(response)}`);
+    if(firstActivityShouldBeScheduled(task.events)) {
+        let response = await scheduleFirstActivity(task);
+        console.log(`schedule activity 1 returns ${JSON.stringify(response)}`);
     }
 };
 
 const  getDeciderWork = async () => {
+    console.log('poll for decision...');
     let response = await swf.pollForDecisionTask({
         domain: workflowDomain,
         taskList: {
@@ -72,12 +90,15 @@ const  getDeciderWork = async () => {
         identity: deciderId
     }).promise();
 
-    if(response.events != undefined) {
-        await performDecisionTask(response);
-    } else {
+    if(response.events == undefined) {
         console.log('nothing to decide');
+        return;
     }
-    
+
+    await Promise.all([performDecisionTask(response), startStepFunctionOrchestration(response)])
+                    .then((results) => {
+                        console.log('startResult', results[1]);
+                    });
 };
 
 const doWork = async () => {
